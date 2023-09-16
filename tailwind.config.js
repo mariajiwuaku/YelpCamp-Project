d68a9b432a0ce58966630338bd8f0adb6949cb37
mutiat <mutiat900@gmail.com>
2023-09-16 11:57:52 +0100

    made corrections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,js}", "./src/**/*.{js,html}"],
   presets: [],
   darkMode: 'media', // or 'class'
   theme: {
