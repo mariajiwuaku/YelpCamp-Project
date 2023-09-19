71158293eaf5532714e0a3b88a20b0b7b5b64427
Maria-goretti Jiwuaku <mariagorettiduruigbo@gmail.com>
2023-09-19 00:17:01 -0700

    created a container for the camp details

---
+++ src/indiv.js
@@ -0,0 +1,21 @@
+//Creating Array for camp details
+
+const campsDetails = [
+    //mount ulap camp
+    {
+      name: "Mount Ulap",
+      price: "104.99 /night",
+      description: "Mount Ulap is a 7.7 Kilometer moderately trafficked point-to-point trail located near Tuba, Benguet, Philippines that offers the chance to see wildlife and is rated as moderate. The trail is primarily used for hiking.",
+      submittedBy: "Andrew Mike",
+      reviews: [
+        { name: "Adam Jones", timeAgo: "13h ago", content: "Honestly one of the best experiences ever." },
+        { name: "Isaac Dylan", timeAgo: "1 day ago", content: "Traveling changes you as a person; you gain more perspective." },
+        { name: "Hudson Luca", timeAgo: "3 days ago", content: "Definitely recommend going there; not too far and not a lot of people to ruin the experience." }
+      ]
+    },
+    //Calaguas Island
+    {
+        name: "Calaguas Island",
+    }
+  ];
+  
