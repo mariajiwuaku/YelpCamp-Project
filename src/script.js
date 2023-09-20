fb33df0f7469240ed9e04b1569df0f8992088ef1
Taofeekat Olorunkosebi <olorunkosebitaofeekat@gmail.com>
2023-09-20 23:16:42 +0100

    working on the search page

--- src/script.js
+++ src/script.js
@@ -5,3 +5,34 @@ const menu = document.querySelector(".mobile-menu");
 btn.addEventListener("click", () => {
     menu.classList.toggle("hidden");
 });
+
+
+
+const searchbar = document.getElementById('searchbar')
+const camps = document.querySelectorAll('#camps ')
+
+searchbar.addEventListener('input', e => {
+    const value = e.target.searchbar.value.toLowercase()
+    camps.forEach(camps => {
+        const isVisible = 
+        camps.innerHTML.toLowerCase().includes(searchbar.value);
+        camps.innerHTML.classList.toggle('hide', !isVisible)
+    })
+})
+
+// function searchCamp() {
+//     let input = document.getElementById('searchbar').value;
+//     input  = input.toLowercase();
+//     let camps = document.querySelector('#camps h5');
+//     camps.innerHTML = input.toLowerCase()
+    
+
+//     for (i = 0; i < camps.length ; i++) {
+//         if (!camps[i].innerHTML.includes(input)) {
+//             camps[i].style.display = "none";
+//         }else {
+//             camps[i].style.display = "block";
+//         }
+//     }
+
+// }
