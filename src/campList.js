6283c00e9816879aa55b95fb61563843a66b35ca
Maria-goretti Jiwuaku <mariagorettiduruigbo@gmail.com>
2023-09-20 23:53:30 -0700

    updates

---
+++ src/campList.js
@@ -0,0 +1,89 @@
+window.campgrounds = [
+    {
+      id: 1,
+      name: "Mount Ulap",
+      description: "One of the most famous hikes in Benguet is Mt Ulap in Itogon.",
+      imageSrc: "./src/Assets/MountUlap.jpg",
+    },
+    {
+      id: 2,
+      name: "Calaguas Island",
+      description: "A paradise of islands that can rival the white sand beauty of Boracay.",
+      imageSrc: "./src/Assets/CalaguasIsland.jpg",
+    },
+    {
+      id: 3,
+      name: "Onay Beach",
+      description: "This is one of the best beach camping sites, beautiful and pristine.",
+      imageSrc: "./src/Assets/OnayBeach.jpg",
+    },
+    {
+      id: 4,
+      name: "Seven Sisters Waterfall",
+      description: "The seven sisters is the 39th tallest waterfall in Norway.",
+      imageSrc: "./src/Assets/SevenSistersWaterfall.jpg",
+    },
+    {
+      id: 5,
+      name: "Latik Riverside",
+      description: "Philippines is one of the most dazzling countries in all of Asia.",
+      imageSrc: "./src/Assets/LatikRiverside.jpg",
+    },
+    {
+      id: 6,
+      name: "Buloy Springs",
+      description: "This is one of the best beach camping sites, beautiful and pristine.",
+      imageSrc: "./src/Assets/BuloySprings.jpg",
+    },
+  ];
+  
+  console.log(campgrounds);
+  
+
+
+// Your search functionality code goes here...
+// Assuming you have an HTML container to display campground cards
+function displayCamps() {
+  // Get the campground container element
+  const campgroundContainer = document.getElementById('camp-container');
+  
+  // Initialize an empty string to accumulate campground cards
+  let campCards = '';
+
+  // Map through the campgrounds array and create HTML elements for each campground
+  campgrounds.forEach((campground) => {
+    campCards += `
+      <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3" class="campground">
+        <div class="rounded-lg border border-black-600">
+          <div class="relative overflow-hidden bg-cover bg-no-repeat" data-te-ripple-init data-te-ripple-color="light">
+            <img class="rounded-t-lg w-full p-4" src="${campground.imageSrc}" alt="${campground.name}" />
+          </div>
+          <div class="p-6">
+            <h5 class="mb-2 text-xl font-bold leading-tight text-neutral-800 dark:text-neutral-50">
+              ${campground.name}
+            </h5>
+            <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+              ${campground.description}
+            </p>
+            <button class="inline-block w-full h-12 px-6 font-bold rounded-lg border border-black-600 text-black-600"
+        onclick="goToDetailsPage(${campground.id})">
+        View Campgrounds
+      </button>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+
+  // Set the innerHTML of the container with all accumulated campground cards
+  campgroundContainer.innerHTML = campCards;
+}
+
+displayCamps();
+
+
+function goToDetailsPage(campgroundId) {
+  // Navigate to the details page passing the campground ID
+  window.location.href = `indiv.html?id=${campgroundId}`;
+}
+
---
+++ src/indiv.js
@@ -0,0 +1,26 @@
+// indiv.js
+const campgrounds = window.campgrounds;
+console.log(campgrounds)
+function displayCampgroundDetails() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const campgroundId = urlParams.get('id');
+  
+    const campground = campgrounds.find((camp) => camp.id === parseInt(campgroundId, 10));
+
+    if (campground) {
+      const campgroundDetailsContainer = document.getElementById('campground-details');
+  
+      const campgroundDetails = `
+        <h2>${campground.name}</h2>
+        <p>${campground.description}</p>
+        <img src="${campground.imageSrc}" alt="${campground.name}" />
+      `;
+  
+      campgroundDetailsContainer.innerHTML = campgroundDetails;
+    } else {
+      console.log("Campground not found");
+    }
+  }
+  
+  displayCampgroundDetails();
+  
